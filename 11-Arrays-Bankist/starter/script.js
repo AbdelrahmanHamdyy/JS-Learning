69d1a998b60a60bc2921cb575bbcf0103255767b
Abdelrahman Hamdy <abdelrahmanhamdy49@gmail.com>
2022-08-15 07:56:46 +0200

    Method Chaining

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -211,3 +211,10 @@ const avg = humanAgeFiltered.reduce(
   0
 );
 console.log(avg); */
+
+const eurToUsd = 1.1;
+const totalDepositsUSD = movements
+  .filter(mov => mov > 0)
+  .map(mov => mov * eurToUsd)
+  .reduce((acc, cur) => acc + cur, 0);
+console.log(totalDepositsUSD);
