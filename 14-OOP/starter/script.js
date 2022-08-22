df309b78366ecfb1fead3af71b56b0e94e9fb128
Abdelrahman Hamdy <abdelrahmanhamdy49@gmail.com>
2022-08-23 00:37:47 +0200

    Constructor Functions

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -12,3 +12,31 @@
 // Objects are linked to a prototypal object
 // Prototypal inheritance -> The prototype contains methods (behaviour) that are accessible to all objects linked to that prototype
 // Ex: Array.prototype is the prototype of all array objects we create
+
+// Constructor Functions
+// Only function declarations and expressions
+const Person = function (firstName, birthYear) {
+  // Instance Properties
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+
+  // Never do this
+  //   this.calcAge = function () {
+  //     console.log(2022 - this.birthYear);
+  //   };
+  // We will instead use prototypes and prototypal inheritance
+};
+
+const me = new Person('Abdelrahman', 2002);
+console.log(me);
+
+// 1. New empty object {} is created
+// 2. Function is called, this = {}
+// 3. {} is linked to a prototype
+// 4. function automatically returns {}
+
+const jonas = new Person('Jonas', 1991);
+const jack = new Person('Jack', 1980);
+console.log(jonas, jack);
+
+console.log(jonas instanceof Person); // True
