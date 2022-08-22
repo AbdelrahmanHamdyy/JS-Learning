e0a2d54614ebb263bbc381b91deb7a619c4b5d1a
Abdelrahman Hamdy <abdelrahmanhamdy49@gmail.com>
2022-08-23 00:20:39 +0200

    Explanation

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,1 +1,14 @@
 'use strict';
+// Abstraction -> Hiding details that don't matter
+// Encapsulation
+// Inheritance
+// Polymorphism
+
+// Objects are instantiated from a class, which functions like a blueprint
+
+//************* OOP IN JS (Prototypes) ***************
+
+// Prototype contains methods and objects can access these methods
+// Objects are linked to a prototypal object
+// Prototypal inheritance -> The prototype contains methods (behaviour) that are accessible to all objects linked to that prototype
+// Ex: Array.prototype is the prototype of all array objects we create
