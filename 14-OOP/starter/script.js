faa2082e1c01960006aa1e3f39dfb9436b5181d1
Abdelrahman Hamdy <abdelrahmanhamdy49@gmail.com>
2022-08-29 02:10:13 +0200

    Account class example

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -390,3 +390,40 @@ const jay = Object.create(StudentProto);
 jay.init('Jay', 2005, 'CE');
 jay.introduce();
 jay.calcAge(); */
+class Account {
+  constructor(owner, currency, pin) {
+    this.owner = owner;
+    this.currency = currency;
+    this.pin = pin;
+    this.movements = [];
+    this.locale = navigator.language;
+    console.log(`Thanks for opening an account, ${owner}`);
+  }
+
+  // Public Interface
+  deposit(val) {
+    this.movements.push(val);
+  }
+
+  withdraw(val) {
+    this.deposit(-val);
+  }
+
+  approveLoan(val) {
+    return true;
+  }
+
+  requestLoan(val) {
+    if (this.approveLoan(val)) {
+      this.deposit(val);
+      console.log('Loan Approved');
+    }
+  }
+}
+
+const acc1 = new Account('Jonas', 'EUR', 1111);
+console.log(acc1);
+acc1.deposit(250);
+acc1.withdraw(140);
+acc1.requestLoan(1000);
+console.log(acc1);
