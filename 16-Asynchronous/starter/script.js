c5d6563319c2c9beb65011b55600b5ee74aa720b
Abdelrahman Hamdy <abdelrahmanhamdy49@gmail.com>
2022-08-30 18:32:07 +0200

    Chaining Promises

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -130,7 +130,15 @@ const getCountry = function (country) {
   // In the beginning, promise is pending. However, let's assume success
   fetch(`https://restcountries.com/v2/name/${country}`)
     .then(response => response.json()) // Async function (Promise)
-    .then(data => renderCountry(data[0]));
+    .then(data => {
+      renderCountry(data[0]);
+      const neighbour = data[0].borders?.[0];
+      return fetch(`https://restcountries.com/v2/alpha/${neighbour}`);
+    })
+    .then(response => response.json())
+    .then(data => renderCountry(data, 'neighbour'));
 };
+
 getCountry('portugal');
+// then returns the success value of the promise
 // Promises do not get rid of callbacks but of callback hell
