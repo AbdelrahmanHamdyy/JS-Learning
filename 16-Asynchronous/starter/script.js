27ed77b061544a0716195b9883efa9de6746de96
Abdelrahman Hamdy <abdelrahmanhamdy49@gmail.com>
2022-08-31 04:49:25 +0200

    Building a Promise and Promisifying setTimout

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -230,3 +230,34 @@ getCountry('NOTFOUND'); */
 //   console.log(res);
 // });
 // console.log('Test end');
+
+const lotteryPromise = new Promise(function (resolve, reject) {
+  // Executer
+  console.log('LOTTERY DRAW 💵');
+  setTimeout(function () {
+    if (Math.random() >= 0.5) {
+      resolve('You WIN 💲💰💲'); // Marks this promise as fulfilled
+    } else {
+      reject(new Error('You lost your money 💩'));
+    }
+  }, 2000);
+});
+
+lotteryPromise.then(res => console.log(res)).catch(err => console.error(err));
+
+// Promisifying setTimeout
+const wait = function (seconds) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, seconds * 1000);
+  });
+};
+
+wait(2)
+  .then(() => {
+    console.log('Waited for 2 seconds');
+    return wait(1);
+  })
+  .then(() => console.log('Waited for 1 second'));
+
+Promise.resolve('abc').then(x => console.log(x));
+Promise.reject(new Error('abc')).catch(x => console.error(x));
