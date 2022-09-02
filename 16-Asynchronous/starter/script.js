e1a4043b1517ac9795a9f3692135b0efa1d3f02b
Abdelrahman Hamdy <abdelrahmanhamdy49@gmail.com>
2022-09-02 05:46:57 +0200

    Async CC#3

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -355,7 +355,7 @@ createImage('img/img-1.jpg')
   })
   .catch(err => console.error(err));
 */
-
+/*
 // Asyn-Await: A simpler and easier way to consume promises
 
 const getPosition = function () {
@@ -488,5 +488,57 @@ Promise.any([
   .then(res => console.log(res))
   .catch(err => console.error(err));
 // Returns the first fulfilled promise and ignores rejected ones
-
+*/
 // CC#3
+const wait = function (seconds) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, seconds * 1000);
+  });
+};
+
+const imgContainer = document.querySelector('.images');
+
+const createImage = function (imgPath) {
+  return new Promise(function (resolve, reject) {
+    const img = document.createElement('img');
+    img.src = imgPath;
+
+    img.addEventListener('load', function () {
+      imgContainer.append(img);
+      resolve(img);
+    });
+
+    img.addEventListener('error', function () {
+      reject(new Error('Image not found'));
+    });
+  });
+};
+
+const loadNPause = async function () {
+  try {
+    let img = await createImage('img/img-1.jpg');
+    await wait(2);
+    img.style.display = none;
+    img = await createImage('img/img-2.jpg');
+    await wait(2);
+    img.style.display = none;
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+// loadNPause();
+
+const loadAll = async function (imgArr) {
+  try {
+    const imgs = imgArr.map(async img => await createImage(img));
+    console.log(imgs);
+    const images = await Promise.all(imgs);
+    console.log(images);
+    images.forEach(img => img.classList.add('parallel'));
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+loadAll(['img/img-1.jpg', 'img/img-2.jpg', 'img/img-3.jpg']);
