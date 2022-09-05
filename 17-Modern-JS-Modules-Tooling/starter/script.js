5834e09b59afe268a753f6f9b2103f10ed650b87
Abdelrahman Hamdy <abdelrahmanhamdy49@gmail.com>
2022-09-05 23:04:46 +0200

    Imports and Exports in practice

--- 17-Modern-JS-Modules-Tooling/starter/script.js
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -1,1 +1,18 @@
 // Importing Module
+// import { addToCart, totalPrice as price, tq } from './shoppingCart';
+// addToCart('bread', 5);
+// console.log(price, tq);
+
+console.log('Importing Module');
+
+// import * as ShoppingCart from './shoppingCart.js';
+// ShoppingCart.addToCart('bread', 5);
+
+// import add, { addToCart, totalPrice as price, tq } from './shoppingCart.js';
+// console.log(price);
+
+// Importing default exports
+import add from './shoppingCart.js';
+add('pizza', 2);
+
+// imports and exports have a live connection
